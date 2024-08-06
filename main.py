2e82cb6f6ad74cff274d627a0c5f36b933cd37cb
javiipardo <114429718+javiipardo@users.noreply.github.com>
2024-08-06 17:30:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
-from scanner import scan_port, scan, os_detection, service_detection, firewall_detection, vulnerability_analysis, report_generation
+from scanner.syn_scan import scan
 
 def main():
     target = input("Ingrese la dirección IP objetivo: ")
-
-    # Escaneo SYN
-    syn_results = scan.scan(target)
+    open_ports = scan(target)
     
-    # Mostrar resultados del escaneo SYN
-    print("Puertos escaneados:")
-    for result in syn_results:
-        print(result)
-
-    # Puedes continuar con otras funciones aquí
-    # os_info = os_detection.detect_os(target)
-    # services = service_detection.detect_services(syn_results)
-    # firewall_info = firewall_detection.detect_firewall(target)
-    # vulnerabilities = vulnerability_analysis.analyze_vulnerabilities(services)
-    # report_generation.generate_report(target, syn_results, os_info, services, firewall_info, vulnerabilities)
+    print("\nPuertos abiertos:")
+    for port in open_ports:
+        print(f"Port {port}")
 
 if __name__ == "__main__":
     main()
